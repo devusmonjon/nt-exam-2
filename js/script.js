2f819e4908763b7f5a2671b17d05e27eac0f75af
usmonjon <umediagram@gmail.com>
2024-06-28 10:22:19 +0500

    add directors section

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
 const menuBtn = document.querySelector(".menu-btn");
 const mobileMenu = document.querySelector(".mobile-menu");
+const directors = document.querySelectorAll(".directors__wrapper");
 
 const dateEl = document.querySelector("#date");
 
 menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("active");
     mobileMenu.classList.toggle("active");
+});
+
+directors.forEach((director) => {
+    const toggleBtn = director.querySelector(".btn");
+
+    toggleBtn.addEventListener("click", () => {
+        if (!director.classList.contains("active")) {
+            directors.forEach((d) => d.classList.remove("active"));
+            director.classList.add("active");
+        } else {
+            director.classList.remove("active");
+        }
+    });
 });
 
 const date = new Date();
